a24f7c109e91c99c01ba7de3d3947d20223d79aa
Sarar Aseer <saseer@stuy.edu>
2019-01-31 12:33:26 -0500

    Commentos

--- 01_Canvas/clickdraw.js
+++ 01_Canvas/clickdraw.js
@@ -24,3 +24,16 @@ function getMousePos(canvas, evt) {
         y: evt.clientY - rect.top
     }
 }
+/*
+preventDefault()
+Eliminates the default function of an object(ex button,checkbox,form). After clicking a button once where the preventDefault() function is triggered, that buttons functionality would become mute.
+
+beginPath()
+Saves the point at which you are drawing lines. It is like if Mario walked down the map, the game would start with beginPath() and save the last position he was at before starting another beginPath()
+
+e.offsetX
+Gives the x-position of the mouse at the instance it is clicked, based on the screen which is a grid of pixels.
+
+e.offsetY
+Gives the y-position of the mouse at the instance it is clicked , based on the screen which is a grid of pixels.
+*/
